00a98b3cbca1ec0d1fbbc6192617a32d479c1d6e
kumail <zdsd0019@gmail.com>
2020-05-31 23:52:39 -0400

    final touch

--- Sprint-3/client/src/Pages/UploadPage/upload.js
+++ Sprint-3/client/src/Pages/UploadPage/upload.js
@@ -1,7 +1,5 @@
 import React from 'react';
-import axios from 'axios';
 import VideoUploadForm from '../../components/VideoUploadForm/VideoUploadForm';
-import VideoListArray from '../../components/VideoListArray/VideoListArray';
 
 
 
--- Sprint-3/server/routes/videosRoutes.js
+++ Sprint-3/server/routes/videosRoutes.js
@@ -2,8 +2,6 @@ const express = require ('express');
 const router = express.Router();
 const videos = require ('./videos.json');
 const nextVideo = require ('./videosListArray.json');
-const uniqid = require('uniqid');
-const fs = require('fs');
 const bodyParser = require('body-parser');
 const cors = require('cors');
 
@@ -24,32 +22,56 @@ router.get('/:id', (request, response) => {
 
 router.post('/', (request, response) => {
 	const uniqueVideo = {
-		// id: uniqid(),
-		// title: request.body.title,
-		// image: request.body.image,
-		// channel: request.body.channel
+		id: request.body.id,
+		title: request.body.title,
+		image: request.body.image,
+		channel: 'Kumail Zaidi'
 		
 	};
-	
-	currentInformation = {
-		id: uniqueVideo.id,
-		title: uniqueVideo.title,
-		image: uniqueVideo.image,
-		channel: uniqueVideo.channel,
-		description: request.body.description,
-		views: request.body.views,
-		likes: request.body.likes,
-		duration: request.body.duration,
-		video: request.body.video,
-		timestamp: request.body.timestamp,
-		comments: []
-	};
 	videos.push(uniqueVideo);
-	nextVideo.push(currentInformation);
-	fs.writeFileSync('./videos.json', JSON.stringify(videos));
-	fs.writeFileSync('./videosListArray.json', JSON.stringify(nextVideo));
 	response.send(uniqueVideo);
-	response.send(nextVideo);
+})
+	
+	router.post('/:id', (request, response) => {
+		const newVideo = {
+			id: request.body.id,
+			title: request.body.title,
+			image: request.body.image,
+			channel: 'Kumail Zaidi',
+				"description": request.body.description,
+				"views": "1,001,023",
+				"likes": "110,985",
+				"duration": "4:01",
+				"video": "https://project-2-api.herokuapp.com/stream",
+				"timestamp": 1545162149000,
+				"comments": [
+					{
+						"name": "Micheal Lyons",
+						"comment": "They BLEW the ROOF off at their last show, once everyone started figuring out they were going. This is still simply the greatest opening of acconcert I have EVER witnessed.",
+						"id": "1ab6d9f6-da38-456e-9b09-ab0acd9ce818",
+						"likes": 0,
+						"timestamp": 1545162149000
+					},
+					{
+						"name": "Gary Wong",
+						"comment": "Every time I see him shred I feel so motivated to get off my couch and hop on my board. He’s so talented! I wish I can ride like him one day so I can really enjoy myself!",
+						"id": "cc6f173d-9e9d-4501-918d-bc11f15a8e14",
+						"likes": 0,
+						"timestamp": 1544595784046
+					},
+					{
+						"name": "Theodore Duncan",
+						"comment": "How can someone be so good!!! You can tell he lives for this and loves to do it every day. Everytime I see him I feel instantly happy! He’s definitely my favorite ever!",
+						"id": "993f950f-df99-48e7-bd1e-d95003cc98f1",
+						"likes": 0,
+						"timestamp": 1542262984046
+					}
+				]
+			
+		};
+
+	nextVideo.push(newVideo);
+	response.send(newVideo);
 })
 
 
